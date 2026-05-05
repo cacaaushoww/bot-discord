28a62b9d7c90cd606a5929c312909e334a0f3f7c
cacaaushoww <cacaaushoww1@gmail.com>
2026-05-05 05:52:53 -0300

    Add guarantee terms message to bot

--- bot.js
+++ bot.js
@@ -7,13 +7,13 @@ const client = new Client({
 // IDs dos canais
 const canalTermos = '1501135306366255134';
 const canalDicas = '1501135727642153092';
+const canalGarantia = '1501136075463196712';
 
 client.once('ready', async () => {
   console.log(`✅ Bot online como ${client.user.tag}`);
 
-  // ===== MENSAGEM TERMOS =====
+  // ===== TERMOS =====
   const canal1 = await client.channels.fetch(canalTermos);
-
   if (canal1) {
     const embedTermos = new EmbedBuilder()
       .setTitle('📚 TERMOS')
@@ -29,7 +29,7 @@ Solicitar reembolso após uso do serviço/produto, exceto em caso de erro confir
 
 **Informações Importantes**
 • Aguardar confirmação da equipe antes de solicitar troca.
-• Não garantimos aprovação ou VBV (verificação bancária).
+• Não garantimos aprovação ou VBV.
 • VPNs ou Proxies podem causar instabilidade.
 • Prefira conexões seguras (4G/5G).
       `)
@@ -38,9 +38,8 @@ Solicitar reembolso após uso do serviço/produto, exceto em caso de erro confir
     canal1.send({ embeds: [embedTermos] });
   }
 
-  // ===== MENSAGEM DICAS =====
+  // ===== DICAS =====
   const canal2 = await client.channels.fetch(canalDicas);
-
   if (canal2) {
     const embedDicas = new EmbedBuilder()
       .setTitle('🔎 Dicas')
@@ -57,6 +56,30 @@ Caso não tenha login bom, adquira ou crie com dados consistentes.
       .setColor('Blue');
 
     canal2.send({ embeds: [embedDicas] });
+  }
+
+  // ===== GARANTIA (ESSA NOVA) =====
+  const canal3 = await client.channels.fetch(canalGarantia);
+  if (canal3) {
+    const embedGarantia = new EmbedBuilder()
+      .setTitle('📜 TERMOS SALDO GARANTIDO')
+      .setDescription(`
+**Trocas**
+• Só fazemos a troca se tiver DIE ou mensagem de saldo insuficiente.
+• Para fazer a troca envie um vídeo mostrando claramente o erro ou a mensagem.
+• Você deve mandar o vídeo antes de 10 minutos após a compra.
+• Passou do prazo de 10m troca será recusada.
+• Se não for mensagem de saldo insuficiente a troca será recusada.
+
+**Lembrando**
+• Não garantimos sua aprovação
+• Não garantimos seu trampo
+• Não garantimos seu esquema
+• Se não cumprir com os termos a troca será recusada, não insista
+      `)
+      .setColor('DarkButNotBlack');
+
+    canal3.send({ embeds: [embedGarantia] });
   }
 });
 
